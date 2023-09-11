dde9d36e7af76457d93b5c2ed03f3fb31526c417
Jason <jason5donoghue@gmail.com>
2023-09-11 22:29:18 +0100

    first name created

---
+++ myName.py
@@ -0,0 +1,12 @@
+myFirst = """
+
+JJJJJ   A    SSS   OOO  N    N
+  J    A A  S   S O   O NN   N
+  J   A   A S     O   O N N  N
+  J   AAAAA  SSS  O   O N  N N
+J J   A   A     S O   O N   NN
+J J   A   A S   S O   O N    N
+ JJ   A   A  SSS   OOO  N    N
+"""
+print(myFirst)
+
