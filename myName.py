1ea59b2214f2b1fe3bca15e9979813cd1d414c58
Jason <jason5donoghue@gmail.com>
2023-09-11 22:35:53 +0100

    second name created

--- myName.py
+++ myName.py
@@ -8,5 +8,16 @@ J J   A   A     S O   O N   NN
 J J   A   A S   S O   O N    N
  JJ   A   A  SSS   OOO  N    N
 """
-print(myFirst)
+# print(myFirst)
 
+mySecond = """
+
+DDDD   OOO  N   N  OOO   GGG  H   H U   U EEEEE
+D   D O   O NN  N O   O G   G H   H U   U E
+D   D O   O N N N O   O G     H   H U   U E
+D   D O   O N  NN O   O GGGGG HHHHH U   U EEE
+D   D O   O N   N O   O G   G H   H U   U E
+D   D O   O N   N O   O G   G H   H U   U E
+DDDD   OOO  N   N  OOO   GGG  H   H  UUU  EEEE
+"""
+print(mySecond)
